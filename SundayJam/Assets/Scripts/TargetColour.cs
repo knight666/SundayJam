0419d814f772f10c7e018c8986a9b1143f37ec66
Quinten Lansu <quintenlansu@gmail.com>
2017-07-09 17:12:52 +0200

    Add color to target location.

--- SundayJam/Assets/Scripts/TargetColour.cs
+++ SundayJam/Assets/Scripts/TargetColour.cs
@@ -6,9 +6,35 @@ using UnityEngine;
 public class TargetColour : MonoBehaviour {
 
 	public string colour = "white";
+	public bool apply_to_renderer = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (apply_to_renderer)
+		{
+			var renderer = GetComponent<SpriteRenderer>();
+
+			if (colour == "red")
+			{
+				renderer.color = Color.red;
+			}
+			else if (colour == "blue")
+			{
+				renderer.color = Color.blue;
+			}
+			else if (colour == "green")
+			{
+				renderer.color = Color.green;
+			}
+			else if (colour == "yellow")
+			{
+				renderer.color = Color.yellow;
+			}
+			else
+			{
+				renderer.color = Color.white;
+			}
+		}
 	}
 	
 	// Update is called once per frame
